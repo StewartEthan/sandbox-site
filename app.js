f11b10e7f342c1af55e3a1c48b0a92b591f5187f
Ethan Edward Stewart <ste11003@byui.edu>
2016-12-03 07:12:39 -0700

    Set up stack and began defining routes

---
+++ app.js
@@ -0,0 +1,40 @@
+// Initial app setup
+var express = require('express'),
+    stylus  = require('stylus');
+
+var app = express();
+
+// Specify the port (I use 3000 as a default for personal dev)
+app.set('port', (/*process.env.PORT ||*/ 9000));
+
+// Specify where static assets should be loaded from
+app.use(express.static('assets'));
+app.use('/assets', express.static('assets'));
+
+// Set the views and views engine
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+
+// Set up routes for the app
+// TODO: Use separate routes.js file???
+// main (root) route
+app.get('/', function(req, res) {
+  res.render('home-page');
+});
+// /apps routes
+app.get('/apps', function(req, res) {
+  res.render('apps');
+});
+app.get('/apps/:appName', function(req, res) {
+  res.render('apps/'+req.params.appName);
+});
+// /code routes
+
+// /blog routes
+
+// /about route
+
+// Start listening
+app.listen(app.get('port'), function() {
+  console.log('Portfolio site is running on port ', app.get('port'));
+});
