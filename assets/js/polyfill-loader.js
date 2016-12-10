42e9b42a186df801084def66d80bbe26011255eb
Ethan Edward Stewart <ste11003@byui.edu>
2016-12-10 14:13:22 -0700

    Created stub file for any polyfill loading needed in the future

---
+++ assets/js/polyfill-loader.js
@@ -0,0 +1,6 @@
+/**
+ * This file will contain any polyfills needed to
+ * maintain reasonable cross browser functionality.
+ * I have no intentions to support IE, but other
+ * major browsers should be supported.
+ */
