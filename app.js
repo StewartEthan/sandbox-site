213f83d72e94d422b2c33ecb86d9348426fbf81f
Ethan Stewart <ste11003@byui.edu>
2017-01-26 01:23:17 -0700

    Updated callbacks to arrow functions and added routing for CIT 261

--- app.js
+++ app.js
@@ -18,28 +18,37 @@ app.set('view engine', 'ejs');
 // Set up routes for the app
 // TODO: Use separate routes.js file???
 // main (root) route
-app.get('/', function(req, res) {
+app.get('/', (req,res) => {
   res.render('home-page');
 });
 // /apps routes
-app.get('/apps', function(req, res) {
+app.get('/apps', (req,res) => {
   res.render('apps');
 });
-app.get('/apps/:appName', function(req, res) {
-  res.render('apps/'+req.params.appName);
+app.get('/apps/:appName', (req,res) => {
+  res.render(`apps/'${req.params.appName}`);
 });
 // /code routes
-app.get('/code', function(req, res) {
+app.get('/code', (req,res) => {
   res.render('code');
 });
 // /blog routes
-app.get('/blog', function(req, res) {
+app.get('/blog', (req,res) => {
   res.render('blog');
 });
 // /about route
-app.get('/about', function(req, res) {
+app.get('/about', (req,res) => {
   res.render('about');
 });
+
+// For CIT 261
+app.get('/cit261', (req,res) => {
+  res.render('cit261/index');
+});
+app.get('/cit261/:topic', (req,res) => {
+  res.render(`cit261/${req.params.topic}`);
+});
+
 // Start listening
 app.listen(app.get('port'), function() {
   console.log('Portfolio site is running on port ', app.get('port'));
