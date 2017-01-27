8691d48a566233d9a7e5291e1d691960d4c0dacb
Ethan Stewart <ste11003@byui.edu>
2017-01-27 02:23:12 -0700

    Added xhr method to utility.js

--- assets/js/utility.js
+++ assets/js/utility.js
@@ -12,6 +12,9 @@ function random(min, max) {
   return min + Math.floor(Math.random() * (max - min));
 }
 
+/**
+ * Provides a smooth scroll to the top of the page
+ */
 function scrollToTop() {
   if (window.scrollTo) {
     window.scrollTo(0,0);
@@ -20,4 +23,45 @@ function scrollToTop() {
       scrollBy(0,-50);
     }
   }
+}
+
+const httpMethods = [
+  'GET',
+  'HEAD',
+  'POST',
+  'PUT',
+  'DELETE',
+  'CONNECT',
+  'OPTIONS'
+]
+function xhr(method, url, headers, data, cb) {
+  // Handle potential errors with params
+  if (httpMethods.indexOf(method) === -1) {
+    throw new Error('You must use a valid HTTP method');
+  }
+  if (typeof url !== "string" || url === '') {
+    throw new Error('url argument must not be empty');
+  }
+  if (headers === undefined) headers = null;
+  if (data === undefined) data = null;
+
+  const xhreq = new XMLHttpRequest();
+
+  xhreq.open(method, url, true);
+
+  xhreq.onreadystatechange = e => {
+    if (xhreq.readyState === XMLHttpRequest.DONE) {
+      // Only use cb if its a function, allowing for it to be optional
+      if (typeof cb === 'function') {
+        cb(xhreq);
+      }
+    }
+  }
+
+  // Set each header
+  for (let key in headers) {
+    xhreq.setRequestHeader(key, headers[key]);
+  }
+
+  xhreq.send(data);
 }
