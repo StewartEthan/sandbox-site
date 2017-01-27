b48609d61d0a82db77b6ffe06f587e0897de4d12
Ethan Stewart <ste11003@byui.edu>
2017-01-27 01:03:11 -0700

    Polyfilled NodeList.prototype.forEach

--- assets/js/polyfill-loader.js
+++ assets/js/polyfill-loader.js
@@ -1,6 +1,17 @@
 /**
  * This file will contain any polyfills needed to
  * maintain reasonable cross browser functionality.
- * I have no intentions to support IE, but other
- * major browsers should be supported.
+ * I have no intentions to go out of my way very far
+ * to support IE, but other major browsers should be supported.
  */
+
+// Polyfill forEach on NodeList objects
+if (!NodeList.prototype.forEach) {
+  NodeList.prototype.forEach = (cb, thisArg) => {
+    if (typeof cb === "function") {
+      for (let i = 0; i < this.length; i++) {
+        cb(this[i], i, this, thisArg);
+      }
+    }
+  }
+}
